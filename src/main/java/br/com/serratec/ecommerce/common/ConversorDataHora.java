70bfa2fd1e5f7a0508b48c532904e03b82bdd63a
Joe <joedsonfera@hotmail.com>
2023-10-14 14:54:14 -0300

    feat: criacao do handler e conversor e data hora

---
+++ src/main/java/br/com/serratec/ecommerce/common/ConversorDataHora.java
@@ -0,0 +1,14 @@
+package br.com.serratec.ecommerce.common;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ConversorDataHora {
+
+    public static String converterDateParaDataHora(Date data) {
+        return new SimpleDateFormat("dd/MM/YYYY HH:mm:ss").format(data);
+    }
+}
+
+// 03/10/2023 20:59:32
+// 2023-10-03T20:59:23
